530c0a7b08701586d86597dee55a495026b7ec4e
Alizah Manag <2019079@icacademy.org>
2017-04-29 23:16:36 +0000

    ScriptEd Hackathon 2017

---
+++ java.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+$("#INeedHelp").click(function(){
+    window.location="INeedHelp.html";
+    }); 
+$("#submit").click(function(){
+    window.location="match.html";
+    });
+$("#IWantToHelp").click(function() {
+    window.location="IWantToHelp.html";
+    });
+$("#submit-2").click(function() {
+    window.location="matchwant.html";
+    });
+$("#person-1").click(function() {
+    window.location="jessicalba.html";
+    });
+$("#person-8").click(function() {
+    window.location="beckyg.html";
+    });
+$("#return").click(function(){
+    window.location="index.html";
+ });
+});
